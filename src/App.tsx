6cb0d027df4c3f62c3fb1a0b80d8cc951d6acc6c
Hannes Güdelhöfer <hannes@guedelhoefer.de>
2022-08-27 19:16:04 +0200

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,91 @@
-import type { Component } from "solid-js"
+import type {Component} from "solid-js";
+import {SwipeImage} from "./components/SwipeImage";
+import {createResource} from "solid-js";
 
-const App: Component = () => {
-  return <h1 class="text-3xl font-bold underline">Hello world!</h1>
+const BACKEND_URL = "https://localhost:5000";
+
+type Picture = {
+    id: string;
+    url: string;
+};
+
+let pictureId = 0
+async function getPicture(): Promise<Picture> {
+    return {
+        id: Math.random() + "",
+        url: pictureId++,
+    }
+    // const response = await fetch(`${BACKEND_URL}/`);
+    // const data = await response.json();
+    // return data;
 }
 
-export default App
+async function votePicture(id: string, value: boolean): Promise<void> {
+    await fetch(`${BACKEND_URL}/vote`, {
+        method: "POST",
+        body: JSON.stringify({
+            id,
+            value,
+        }),
+    });
+}
+
+async function getPictures(_:unknown, info: { value?: Array<Picture> }) {
+    console.log("refetching!")
+    console.dir(info)
+    const toFetch = 5 - (info?.value?.length ?? 0)
+    console.dir(toFetch)
+    const fetched = await Promise.all(
+        Array(toFetch)
+            .fill(0)
+            .map(() => getPicture())
+    );
+    return [
+        ...(info?.value ?? []),
+        ...fetched.filter((it) => info?.value?.every((pic) => pic.id !== it.id) ?? true),
+    ];
+}
+
+const [pictures, {mutate: mutatePictures, refetch: refetchPictures}] =
+    createResource(getPictures);
+
+async function onNegative(id: string) {
+    const [first, ...rest] = pictures() ?? [];
+    mutatePictures((info) => {
+        return rest
+    })
+    try {
+        await votePicture(id, false);
+    } catch (e) {
+    }
+    refetchPictures({value: rest});
+}
+
+async function onPositive(id: string) {
+    const [first, ...rest] = pictures() ?? [];
+    mutatePictures((info) => {
+        const [first, ...rest] = info ?? [];
+        return rest
+    })
+    try {
+        await votePicture(id, false);
+    } catch (e) { }
+    refetchPictures({value: rest});
+}
+
+const App: Component = () => {
+    return (
+        <>
+            {pictures()?.map((pic, index) => (
+                <SwipeImage
+                    src={pic.url}
+                    onPositive={() => onPositive(pic.id)}
+                    onNegative={() => onNegative(pic.id)}
+                    index={index}
+                />
+            ))}
+        </>
+    );
+};
+
+export default App;
---
+++ src/components/SwipeImage.tsx
@@ -0,0 +1,51 @@
+import { Component, createSignal, onMount } from "solid-js";
+import Hammer from "hammerjs";
+
+const THRESHOLD = 200;
+export const SwipeImage: Component<{
+    src: string;
+    onPositive: () => void;
+    onNegative: () => void;
+    index: number;
+}> = ({ src, onPositive, onNegative, index }) => {
+    let imageRef: HTMLImageElement | undefined;
+    const [xOffset, setXOffset] = createSignal(0);
+    const [yOffset, setYOffset] = createSignal(0);
+    onMount(() => {
+        const mc = new Hammer(imageRef);
+        mc.on("panleft panright panend", function (ev) {
+            if (ev.type === "panend") {
+                setXOffset(0);
+                setYOffset(0);
+                if (ev.deltaX > THRESHOLD) {
+                    onPositive();
+                }
+
+                if (ev.deltaX < -THRESHOLD) {
+                    onNegative();
+                }
+
+                return;
+            }
+
+            setXOffset(-ev.deltaX);
+            setYOffset(ev.deltaY);
+        });
+    });
+    return (
+        <div
+            ref={imageRef}
+            src={src}
+            style={{
+                width: "100vw",
+                transition: "all 0.01s ease",
+
+                right: `${xOffset()}px`,
+                top: `${yOffset()}px`,
+                position: "absolute",
+                zIndex: 5 - index,
+                background: "white"
+            }}
+        >{src}</div>
+    );
+};
