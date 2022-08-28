b8991f8fd74dabaa306ec86a589adfbe271aef01
Hilmar Wiegand <me@hwgnd.de>
2022-08-28 18:15:38 +0200

    Revert to tab width 2

--- src/components/SwipeImage.tsx
+++ src/components/SwipeImage.tsx
@@ -1,51 +1,53 @@
-import { Component, createSignal, onMount } from "solid-js";
-import Hammer from "hammerjs";
+import { Component, createSignal, onMount } from "solid-js"
+import Hammer from "hammerjs"
 
-const THRESHOLD = 200;
+const THRESHOLD = 200
 export const SwipeImage: Component<{
-    src: string;
-    onPositive: () => void;
-    onNegative: () => void;
-    index: number;
+  src: string
+  onPositive: () => void
+  onNegative: () => void
+  index: number
 }> = ({ src, onPositive, onNegative, index }) => {
-    let imageRef: HTMLImageElement | undefined;
-    const [xOffset, setXOffset] = createSignal(0);
-    const [yOffset, setYOffset] = createSignal(0);
-    onMount(() => {
-        const mc = new Hammer(imageRef);
-        mc.on("panleft panright panend", function (ev) {
-            if (ev.type === "panend") {
-                setXOffset(0);
-                setYOffset(0);
-                if (ev.deltaX > THRESHOLD) {
-                    onPositive();
-                }
+  let imageRef: HTMLImageElement | undefined
+  const [xOffset, setXOffset] = createSignal(0)
+  const [yOffset, setYOffset] = createSignal(0)
+  onMount(() => {
+    const mc = new Hammer(imageRef)
+    mc.on("panleft panright panend", function (ev) {
+      if (ev.type === "panend") {
+        setXOffset(0)
+        setYOffset(0)
+        if (ev.deltaX > THRESHOLD) {
+          onPositive()
+        }
 
-                if (ev.deltaX < -THRESHOLD) {
-                    onNegative();
-                }
+        if (ev.deltaX < -THRESHOLD) {
+          onNegative()
+        }
 
-                return;
-            }
+        return
+      }
 
-            setXOffset(-ev.deltaX);
-            setYOffset(ev.deltaY);
-        });
-    });
-    return (
-        <div
-            ref={imageRef}
-            src={src}
-            style={{
-                width: "100vw",
-                transition: "all 0.01s ease",
+      setXOffset(-ev.deltaX)
+      setYOffset(ev.deltaY)
+    })
+  })
+  return (
+    <div
+      ref={imageRef}
+      src={src}
+      style={{
+        width: "100vw",
+        transition: "all 0.01s ease",
 
-                right: `${xOffset()}px`,
-                top: `${yOffset()}px`,
-                position: "absolute",
-                zIndex: 5 - index,
-                background: "white"
-            }}
-        >{src}</div>
-    );
-};
+        right: `${xOffset()}px`,
+        top: `${yOffset()}px`,
+        position: "absolute",
+        zIndex: 5 - index,
+        background: "white",
+      }}
+    >
+      {src}
+    </div>
+  )
+}
